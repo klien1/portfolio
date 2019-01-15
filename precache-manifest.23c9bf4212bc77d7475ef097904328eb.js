e8c635085ae4b10221e7a8e14047f97ad13151b1
Kevin Lien <klien8589@gmail.com>
2019-01-15 08:16:44 -0800

    Updates

---
+++ precache-manifest.23c9bf4212bc77d7475ef097904328eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "79e380e35e537005989a",
+    "url": "/portfolio/static/js/main.79e380e3.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/portfolio/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "79e380e35e537005989a",
+    "url": "/portfolio/static/css/main.c526e706.chunk.css"
+  },
+  {
+    "revision": "47d75c54d64924d2d2306234b46a532f",
+    "url": "/portfolio/index.html"
+  }
+];
